afe7cbc020d30748ade6d90074ea6703fe67def6
raphaelmazor <raphaelm@sccwrp.org>
2020-08-18 12:47:38 -0700

    Updated chem translator

--- SMC_Chem_Translator_v1.R
+++ SMC_Chem_Translator_v1.R
@@ -15,8 +15,55 @@ CEDEN_chem_labbatch<-chem_batch.df %>%
             )
 
 CEDEN_chem_chemresults<-chem_results.df %>%
-  # filter() %>% #Eventually add code to filter out entries we don't want to deal with
-  transmute(StationCode=stationcode,
+  # filter() %>% #Eventually add code to filter out entries we don't want to deal withleft_join(chem_batch.df %>%
+  left_join(CEDEN_chem_labbatch<-chem_batch.df %>%
+              select(labbatch,preparationpreservationname,preparationpreservationdate,digestextractmethod)) %>%
+    transmute(StationCode=stationcode,
              SampleDate=sampledate,
-             ProjectCode=paste0("SMC_",login_owner, login_year)
-             )
+             # ProjectCode=paste0("SMC_",login_owner, login_year),
+            ProjectCode="SMC", #Maybe one project for all?
+            EventCode="BA",
+            ProtocolCode="SWAMP_2016_WS",
+            AgencyCode=labagencycode,
+            SampleComments=labresultcomments,
+            LocationCode="Bank",
+            GeometryShape="Point",
+            CollectionTime="00:00", #Value in guidance for unknown
+            CollectionMethodCode=case_when(matrixname %in% c("samplewater","samplewater, <1.2 um")~"Water_Grab",
+                                           matrixname %in% c("sediment")~"Sed_Grab",
+                                           matrixname %in% c("labwater","blankwater","blankmatrix")~"Not Applicable",
+                                           matrixname %in% c("benthic")~"Algae_SWAMP",
+                                           T~"FLAG"), #Probably "Not Applicable"
+            SampleTypeCode=sampletypecode,
+            Replicate=fieldreplicate,
+            CollectionDeviceName="Not Recorded",
+            CollectionDepth= -88,
+            UnitCollectionDepth="m",
+            PositionWaterColumn="",
+            LabCollectionComments="",
+            LabBatch=labbatch,
+            AnalysisDate=analysisdate,
+            MatrixName=matrixname,
+            MethodName=methodname,
+            AnalyteName=analytename,
+            FractionName=fractionname,
+            UnitName=unit,
+            LabReplicate=labreplicate,
+            Result=result,
+            ResQualCode=resqualcode,
+            MDL=mdl,	
+            RL=rl,
+            QACode=qacode,
+            ComplianceCode="",
+            DilutionFactor=dilfactor,
+            ExpectedValue=expectedvalue,
+            PrepPreservationName=preparationpreservationname,
+            PrepPreservationDate=preparationpreservationdate,
+            DigestExtractMethod=digestextractmethod,
+            DigestExtractDate="01/Jan/1950 00:00",
+            SampleID="",
+            LabSampleID=labsampleid,
+            LabResultComments=labresultcomments
+          )
+            		
+             
