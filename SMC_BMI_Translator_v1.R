18591185d939c8e6a506a7aec72b604906c37023
annieholt <anneh@sccwrp.org>
2020-08-21 18:32:14 -0700

    Annie data wrangling updates

--- SMC_BMI_Translator_v1.R
+++ SMC_BMI_Translator_v1.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-# aNNIE will update csci portion
+
 
 load("Data/bmi_csci.Rdata")
 # list(lu_station.df, bmi_tax_results.df, csci_core.df, csci_suppl1_grps.df, csci_suppl1_mmi.df, csci_suppl1_oe.df)
@@ -11,6 +11,17 @@ csci_core.df<-bmi_csci[[4]]
 csci_suppl1_grps.df<-bmi_csci[[5]]
 csci_suppl1_mmi.df<-bmi_csci[[6]]
 csci_suppl1_oe.df<-bmi_csci[[7]]
+
+# making csci_suppl1_oe wide format to join with csci core, mmi, grps, then pivot long after
+# this will give us sampledate, collectionmethodcode, and field replicate information from csci_core for all csci data
+csci_suppl1_oe_wide.df <- csci_suppl1_oe.df %>% 
+  select(sampleid, otu, captureprob) %>% 
+  mutate(otu=paste0("Pc_",otu)) %>% 
+  pivot_wider(names_from = "otu", values_from = "captureprob") %>% 
+  rename(SampleID = sampleid)
+
+
+
 
 
 CEDEN_benthic_locations<-bmi_tax_sampleinfo.df %>%
@@ -107,94 +118,126 @@ CEDEN_benthic_benthicresults<-bmi_tax_sampleinfo.df %>%
 CEDEN_habitat_csci_part1<-
   csci_core.df %>%
   filter(record_origin=="SMC") %>%
-  select(sampleid, stationcode, sampledate, collectionmethodcode, fieldreplicate, count, number_of_mmi_iterations, number_of_oe_iterations, pcnt_ambiguous_individuals, pcnt_ambiguous_taxa, e, mean_o, oovere, mmi, csci, csci_percentile)
+  select(sampleid, stationcode, sampledate, collectionmethodcode, fieldreplicate, count, number_of_mmi_iterations,
+         number_of_oe_iterations, pcnt_ambiguous_individuals, pcnt_ambiguous_taxa,
+         e, mean_o, oovere, mmi, csci, csci_percentile) %>% 
   inner_join(csci_suppl1_mmi.df %>% select(sampleid, clinger_percenttaxa, clinger_percenttaxa_predicted, clinger_percenttaxa_score, coleoptera_percenttaxa, coleoptera_percenttaxa_predict, coleoptera_percenttaxa_score, taxonomic_richness, taxonomic_richness_predicted, taxonomic_richness_score, ept_percenttaxa, ept_percenttaxa_predicted, ept_percenttaxa_score, shredder_taxa, shredder_taxa_predicted, shredder_taxa_score, intolerant_percent, intolerant_percent_predicted, intolerant_percent_score), 
              by="sampleid") %>%
   inner_join(csci_suppl1_grps.df %>% select(stationcode, pgroup1, pgroup2, pgroup3, pgroup4, pgroup5, pgroup6, pgroup7, pgroup8, pgroup9, pgroup10,pgroup11), 
-             by="stationcode") %>%
+             by="stationcode") %>% 
   # select(-mmi_percentile, -oovere_percentile, -mmi_score) %>% 
   rename(SampleID=tolower("SampleID"),StationCode=tolower("StationCode"), SampleDate=tolower("SampleDate"),CollectionMethodCode=tolower("CollectionMethodCode"),
          FieldReplicate=tolower("FieldReplicate"),Count=tolower("Count"), Number_of_MMI_Iterations=tolower("Number_of_MMI_Iterations"),Number_of_OE_Iterations=tolower("Number_of_OE_Iterations"), 
-         Pcnt_Ambiguous_Individuals=tolower("Pcnt_Ambiguous_Individuals"),Pcnt_Ambiguous_Taxa=tolower("Pcnt_Ambiguous_Taxa"),E="e",Mean_O=tolower("Mean_O"),OoverE=tolower("OoverE"),CSCI="csci",CSCI_Percentile=tolower("CSCI_Percentile"),
+         Pcnt_Ambiguous_Individuals=tolower("Pcnt_Ambiguous_Individuals"),Pcnt_Ambiguous_Taxa=tolower("Pcnt_Ambiguous_Taxa"),E="e",Mean_O=tolower("Mean_O"),OoverE=tolower("OoverE"),MMI = "mmi",CSCI="csci",CSCI_Percentile=tolower("CSCI_Percentile"),
          Clinger_PercentTaxa=tolower("Clinger_PercentTaxa"), Clinger_PercentTaxa_predicted=tolower("Clinger_PercentTaxa_predicted"), Clinger_PercentTaxa_score=tolower("Clinger_PercentTaxa_score"), 
-         Coleoptera_PercentTaxa=tolower("Coleoptera_PercentTaxa"), Coleoptera_PercentTaxa_predicted=tolower("Coleoptera_PercentTaxa_predicted"), Coleoptera_PercentTaxa_score=tolower("Coleoptera_PercentTaxa_score"), 
+         Coleoptera_PercentTaxa=tolower("Coleoptera_PercentTaxa"), Coleoptera_PercentTaxa_predicted=tolower("Coleoptera_PercentTaxa_predict"), Coleoptera_PercentTaxa_score=tolower("Coleoptera_PercentTaxa_score"), 
          Taxonomic_Richness=tolower("Taxonomic_Richness"), Taxonomic_Richness_predicted=tolower("Taxonomic_Richness_predicted"), Taxonomic_Richness_score=tolower("Taxonomic_Richness_score"), 
          EPT_PercentTaxa=tolower("EPT_PercentTaxa"), EPT_PercentTaxa_predicted=tolower("EPT_PercentTaxa_predicted"), EPT_PercentTaxa_score=tolower("EPT_PercentTaxa_score"), 
          Shredder_Taxa=tolower("Shredder_Taxa"), Shredder_Taxa_predicted=tolower("Shredder_Taxa_predicted"), Shredder_Taxa_score=tolower("Shredder_Taxa_score"), 
          Intolerant_Percent=tolower("Intolerant_Percent"), Intolerant_Percent_predicted=tolower("Intolerant_Percent_predicted"), Intolerant_Percent_score=tolower("Intolerant_Percent_score"),
          
          
-         )%>%
-  pivot_longer(cols = c(
-    #From core report
-    Count,Number_of_MMI_Iterations,Number_of_OE_Iterations, Pcnt_Ambiguous_Individuals,Pcnt_Ambiguous_Taxa,
-    E,Mean_O,OoverE,CSCI,CSCI_Percentile,
-    #From Suppl1_mmi
-    Clinger_PercentTaxa, Clinger_PercentTaxa_predicted, Clinger_PercentTaxa_score, 
-    Coleoptera_PercentTaxa, Coleoptera_PercentTaxa_predicted, Coleoptera_PercentTaxa_score, 
-    Taxonomic_Richness, Taxonomic_Richness_predicted, Taxonomic_Richness_score, 
-    EPT_PercentTaxa, EPT_PercentTaxa_predicted, EPT_PercentTaxa_score, 
-    Shredder_Taxa, Shredder_Taxa_predicted, Shredder_Taxa_score, 
-    Intolerant_Percent, Intolerant_Percent_predicted, Intolerant_Percent_score,
-    #From Suppl1_grps
-    pGroup1, pGroup2, pGroup3, pGroup4, pGroup5, pGroup6, pGroup7, pGroup8, pGroup9, pGroup10, pGroup11),
-    names_to = "AnalyteName",
-    values_to = "Result") %>%
+         ) %>% 
+  rename(pGroup1 = pgroup1, pGroup2 = pgroup2, pGroup3 = pgroup3, pGroup4 = pgroup4, pGroup5 = pgroup5, pGroup6 = pgroup6,
+         pGroup7 = pgroup7, pGroup8 = pgroup8, pGroup9 = pgroup9, pGroup10 = pgroup10, pGroup11 = pgroup11) %>% 
+  inner_join(csci_suppl1_oe_wide.df, by = "SampleID") %>% #how I decided to join oe data, instead of dealing with it separately 
+  
+  pivot_longer(cols = c(-SampleID, -StationCode, -SampleDate,-CollectionMethodCode,-FieldReplicate),
+               names_to = "AnalyteName",
+               values_to = "Result") %>% 
+  # previous version of pivot longer below, no longer using this because also added oe data before the pivot
+  # note that I can designate which columns not to pivot wide above, instead of listing all the columns like below method
+  
+  # pivot_longer(cols = c(
+  #   #From core report
+  #   Count,Number_of_MMI_Iterations,Number_of_OE_Iterations, Pcnt_Ambiguous_Individuals,Pcnt_Ambiguous_Taxa,
+  #   E,Mean_O,OoverE,CSCI,CSCI_Percentile,
+  #   #From Suppl1_mmi
+  #   Clinger_PercentTaxa, Clinger_PercentTaxa_predicted, Clinger_PercentTaxa_score, 
+  #   Coleoptera_PercentTaxa, Coleoptera_PercentTaxa_predicted, Coleoptera_PercentTaxa_score, 
+  #   Taxonomic_Richness, Taxonomic_Richness_predicted, Taxonomic_Richness_score, 
+  #   EPT_PercentTaxa, EPT_PercentTaxa_predicted, EPT_PercentTaxa_score, 
+  #   Shredder_Taxa, Shredder_Taxa_predicted, Shredder_Taxa_score, 
+  #   Intolerant_Percent, Intolerant_Percent_predicted, Intolerant_Percent_score,
+  #   #From Suppl1_grps
+  #   pGroup1, pGroup2, pGroup3, pGroup4, pGroup5, pGroup6, pGroup7, pGroup8, pGroup9, pGroup10, pGroup11),
+  #   names_to = "AnalyteName",
+  #   values_to = "Result") %>% 
+
   mutate(AnalyteName = case_when(AnalyteName=="CSCI_Percentile"~"CSCI_Percentile", #The only variable that doesn't follow this naming convention
                                  AnalyteName=="CSCI"~"CSCI", #The only variable that doesn't follow this naming convention
                                  T~paste0("CSCI_", AnalyteName)))
+
+
+
 #Many fields require manual filling. These are left as blank ("") for manual entry in Excel.
-CEDENify_CSCI<-function(core=core, Suppl1_mmi=Suppl1_mmi, ){
+
+# this function contains the same steps for CEDEN_habitat_csci_part1, but was continued to include addition sample information 
+# some udpates may still need to be made to the sample information fields, including additions from the BMI dataset and PHAB
+# might be nice to just have one or two functions..could also change benthic sections into a function
+
+CEDENify_CSCI<-function(core=csci_core.df, Suppl1_mmi=csci_suppl1_mmi.df,Suppl1_grps = csci_suppl1_grps.df,
+                        Suppl1_OE = csci_suppl1_oe.df){
   #Core, Suppl1_mmi, and Suppl1_grps
-  all_but_oe<-csci_core %>%
-    left_join(csci_Suppl1_mmi) %>%
-    left_join(csci_Suppl1_grps) %>%
-    select(-MMI_Percentile, -OoverE_Percentile, -MMI_Score) %>%
+  all_csci<-csci_core.df %>%
+    filter(record_origin=="SMC") %>%
+    select(sampleid, stationcode, sampledate, collectionmethodcode, fieldreplicate, count, number_of_mmi_iterations,
+           number_of_oe_iterations, pcnt_ambiguous_individuals, pcnt_ambiguous_taxa,
+           e, mean_o, oovere, mmi, csci, csci_percentile) %>% 
+    left_join(csci_suppl1_mmi.df %>% select(sampleid, clinger_percenttaxa, clinger_percenttaxa_predicted, clinger_percenttaxa_score, coleoptera_percenttaxa,
+                                 coleoptera_percenttaxa_predict, coleoptera_percenttaxa_score, taxonomic_richness, taxonomic_richness_predicted,
+                                 taxonomic_richness_score, ept_percenttaxa, ept_percenttaxa_predicted, ept_percenttaxa_score, shredder_taxa,
+                                 shredder_taxa_predicted, shredder_taxa_score, intolerant_percent, intolerant_percent_predicted, intolerant_percent_score), 
+              by="sampleid") %>%
+    left_join(csci_suppl1_grps.df%>% select(stationcode, pgroup1, pgroup2, pgroup3, pgroup4, pgroup5, pgroup6, pgroup7, pgroup8, pgroup9, pgroup10,pgroup11), 
+              by="stationcode") %>%
+    # select(-MMI_Percentile, -OoverE_Percentile, -MMI_Score) %>%
     # names() %>%dput()
-    pivot_longer(cols = c(
-      #From core report
-      Count,Number_of_MMI_Iterations,Number_of_OE_Iterations, Pcnt_Ambiguous_Individuals,Pcnt_Ambiguous_Taxa,
-      E,Mean_O,OoverE,CSCI,CSCI_Percentile,
-      #From Suppl1_mmi
-      Clinger_PercentTaxa, Clinger_PercentTaxa_predicted, Clinger_PercentTaxa_score, 
-      Coleoptera_PercentTaxa, Coleoptera_PercentTaxa_predicted, Coleoptera_PercentTaxa_score, 
-      Taxonomic_Richness, Taxonomic_Richness_predicted, Taxonomic_Richness_score, 
-      EPT_PercentTaxa, EPT_PercentTaxa_predicted, EPT_PercentTaxa_score, 
-      Shredder_Taxa, Shredder_Taxa_predicted, Shredder_Taxa_score, 
-      Intolerant_Percent, Intolerant_Percent_predicted, Intolerant_Percent_score,
-      #From Suppl1_grps
-      pGroup1, pGroup2, pGroup3, pGroup4, pGroup5, pGroup6, pGroup7, pGroup8, pGroup9, pGroup10, pGroup11
-    ),
-    names_to = "AnalyteName",
-    values_to = "Result") %>%
+    rename(SampleID=tolower("SampleID"),StationCode=tolower("StationCode"), SampleDate=tolower("SampleDate"),CollectionMethodCode=tolower("CollectionMethodCode"),
+           FieldReplicate=tolower("FieldReplicate"),Count=tolower("Count"), Number_of_MMI_Iterations=tolower("Number_of_MMI_Iterations"),Number_of_OE_Iterations=tolower("Number_of_OE_Iterations"), 
+           Pcnt_Ambiguous_Individuals=tolower("Pcnt_Ambiguous_Individuals"),Pcnt_Ambiguous_Taxa=tolower("Pcnt_Ambiguous_Taxa"),E="e",Mean_O=tolower("Mean_O"),OoverE=tolower("OoverE"),MMI = "mmi",CSCI="csci",CSCI_Percentile=tolower("CSCI_Percentile"),
+           Clinger_PercentTaxa=tolower("Clinger_PercentTaxa"), Clinger_PercentTaxa_predicted=tolower("Clinger_PercentTaxa_predicted"), Clinger_PercentTaxa_score=tolower("Clinger_PercentTaxa_score"), 
+           Coleoptera_PercentTaxa=tolower("Coleoptera_PercentTaxa"), Coleoptera_PercentTaxa_predicted=tolower("Coleoptera_PercentTaxa_predict"), Coleoptera_PercentTaxa_score=tolower("Coleoptera_PercentTaxa_score"), 
+           Taxonomic_Richness=tolower("Taxonomic_Richness"), Taxonomic_Richness_predicted=tolower("Taxonomic_Richness_predicted"), Taxonomic_Richness_score=tolower("Taxonomic_Richness_score"), 
+           EPT_PercentTaxa=tolower("EPT_PercentTaxa"), EPT_PercentTaxa_predicted=tolower("EPT_PercentTaxa_predicted"), EPT_PercentTaxa_score=tolower("EPT_PercentTaxa_score"), 
+           Shredder_Taxa=tolower("Shredder_Taxa"), Shredder_Taxa_predicted=tolower("Shredder_Taxa_predicted"), Shredder_Taxa_score=tolower("Shredder_Taxa_score"), 
+           Intolerant_Percent=tolower("Intolerant_Percent"), Intolerant_Percent_predicted=tolower("Intolerant_Percent_predicted"), Intolerant_Percent_score=tolower("Intolerant_Percent_score"),
+           pGroup1 = pgroup1, pGroup2 = pgroup2, pGroup3 = pgroup3, pGroup4 = pgroup4, pGroup5 = pgroup5, pGroup6 = pgroup6,
+           pGroup7 = pgroup7, pGroup8 = pgroup8, pGroup9 = pgroup9, pGroup10 = pgroup10, pGroup11 = pgroup11
+           ) %>% 
+    inner_join(csci_suppl1_oe_wide.df, by = "SampleID") %>% #how I decided to join oe data, instead of dealing with it separately 
+    pivot_longer(cols = c(-SampleID, -StationCode, -SampleDate,-CollectionMethodCode,-FieldReplicate),
+                 names_to = "AnalyteName",
+                 values_to = "Result") %>% 
+    # note that I can designate which columns not to pivot wide above, instead of listing all the columns like previous method
     mutate(AnalyteName = case_when(AnalyteName=="CSCI_Percentile"~"CSCI_Percentile", #The only variable that doesn't follow this naming convention
                                    AnalyteName=="CSCI"~"CSCI", #The only variable that doesn't follow this naming convention
                                    T~paste0("CSCI_", AnalyteName)))
-  #Do Suppl1_OE separately because it's already in long format and because it requires different analyte renaming.
-  just_oe<-csci_Suppl1_OE %>%
-    select(StationCode, SampleID, AnalyteName=OTU, Result=CaptureProb) %>%
-    mutate(AnalyteName=paste0("CSCI_Pc_",AnalyteName))
-  #Bind, add other SWAMP fields, and re-order
-  xdf<-bind_rows(all_but_oe, just_oe) %>%
-    mutate(SampleDate="",
+ 
+  #add other SWAMP fields, and re-order
+  # still need to get soem information from phab and perhaps bmi
+  
+  xdf<-all_csci %>% 
+    mutate(SampleDate=SampleDate,#from csci core
            ProjectCode="",
            EventCode="BA",
            ProtocolCode="", #Or always "SWAMP_2016_WS"?
-           AgencyCode="",
+           AgencyCode="", #get from bmi_tax_sampleinfo??
            SampleComments="",
            LocationCode="X",
            GeometryShape="Point",
            CollectionTime="",
-           CollectionMethodCode="",
-           Replicate="",
+           CollectionMethodCode=CollectionMethodCode, #from csci core
+           Replicate=FieldReplicate, #from csci core
            HabitatCollectionComments="",
            MatrixName="benthic",
-           MethodName=paste0("CSCI_software_v",strsplit(packageVersion("CSCI") %>% as.character(),split=".")[[1]][1],".x"),
+           MethodName = "CSCI_software_v1.x",
+           # MethodName=paste0("CSCI_software_v",strsplit(packageVersion("CSCI") %>% as.character(),split=".")[[1]][1],".x"),
            FractionName="None",
            UnitName="none",
            VariableResult="",
            ResQualCode="=",
-           QACode="None",
+           QACode="None", #get from bmi_tax_sampleinfo??
            ComplianceCode="Pend",
            BatchVerificationCode="NR",
            CollectionDeviceName="D-Frame Kick Net", #Or do we want to leave blank?
